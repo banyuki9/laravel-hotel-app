f12f3620b8f1980714d1905da7667fad15a9510c
banyuki9 <yuki1999ban@gmail.com>
2022-09-04 22:19:04 +0900

    客室のfactoryとseederを作成

---
+++ database/seeders/RoomsSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Rooms;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoomsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Rooms::factory()->count(5)->create();
+    }
+}
