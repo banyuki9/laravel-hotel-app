57ffb88c3836252ed3bfa98423b956f05cf1182d
banyuki9 <yuki1999ban@gmail.com>
2022-10-07 14:23:30 +0900

    ヘッダーの修正

--- resources/js/Components/Nav.vue
+++ resources/js/Components/Nav.vue
@@ -1,13 +1,12 @@
 <script setup>
-  import BreezeDropdownLink from '@/Components/DropdownLink.vue';
+import BreezeDropdownLink from "@/Components/DropdownLink.vue";
+import BreezeApplicationLogo from "@/Components/ApplicationLogo.vue";
 import { ref } from "vue";
 const hasMenu = ref(false);
 
 const toggleMenu = () => {
   hasMenu.value ? (hasMenu.value = false) : (hasMenu.value = true);
 };
-
-
 </script>
 
 <template>
@@ -71,7 +70,7 @@ const toggleMenu = () => {
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
             <!-- Profile dropdown -->
-            <div class="relative ml-3">
+            <div class="relative ml-3" v-if="$page.props.auth.user">
               <div>
                 <button
                   type="button"
@@ -94,10 +93,8 @@ const toggleMenu = () => {
                   @click="toggleMenu"
                 >
                   <span class="sr-only">Open user menu</span>
-                  <img
-                    class="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
+                  <BreezeApplicationLogo
+                    class="w-10 h-10 fill-current text-gray-500"
                   />
                 </button>
               </div>
@@ -140,7 +137,7 @@ const toggleMenu = () => {
                   role="menuitem"
                   tabindex="-1"
                   id="user-menu-item-0"
-                  >Your Profile</a
+                  >ご予約</a
                 >
 
                 <a
@@ -149,7 +146,7 @@ const toggleMenu = () => {
                   role="menuitem"
                   tabindex="-1"
                   id="user-menu-item-1"
-                  >Settings</a
+                  >設定</a
                 >
 
                 <BreezeDropdownLink
@@ -157,9 +154,40 @@ const toggleMenu = () => {
                   method="post"
                   as="button"
                 >
-                  Log Out
+                  ログアウト
                 </BreezeDropdownLink>
               </div>
+            </div>
+
+            <div v-else>
+              <a
+                :href="route('login')"
+                class="
+                  text-gray-300
+                  hover:bg-gray-700 hover:text-white
+                  px-3
+                  py-2
+                  rounded-md
+                  text-sm
+                  font-medium
+                "
+              >
+                ログイン
+              </a>
+              <a
+                :href="route('register')"
+                class="
+                  text-gray-300
+                  hover:bg-gray-700 hover:text-white
+                  px-3
+                  py-2
+                  rounded-md
+                  text-sm
+                  font-medium
+                "
+              >
+                新規登録
+              </a>
             </div>
           </div>
         </div>
