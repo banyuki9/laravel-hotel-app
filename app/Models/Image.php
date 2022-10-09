f0854716e7076a7f1e822fc700d22bc5e87c5d15
banyuki9 <yuki1999ban@gmail.com>
2022-10-09 18:56:10 +0900

    画像の表示機能の実装

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+    protected $appends = ['image_url'];
+    
+    public function getImageUrlAttribute()
+    {
+        return \Storage::url('images/' . $this->link); // storageフォルダを起点としてファイルパスを生成してreturnする
+    }
 }
