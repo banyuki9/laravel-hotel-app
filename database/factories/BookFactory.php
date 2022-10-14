033540154ea451eda50a7d6e4804c539cf96398e
banyuki9 <yuki1999ban@gmail.com>
2022-10-14 13:27:31 +0900

    BookFactoryの修正

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -4,6 +4,9 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Carbon\Carbon;
+use App\Models\Plan;
+use App\Models\User;
+use App\Services\Book\BookService;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
@@ -17,19 +20,19 @@ class BookFactory extends Factory
      */
     public function definition()
     {
+        $bookService = new BookService();
         return [
             'user_id' => 1,
-            'plan_id' => 1,
+            'plan_id' => Plan::factory(),
             'note' => '備考が入ります。備考が入ります。',
             'adult_number' => 2,
             'child_number' => 0,
             'total_amount' => 14000,
             'stripe_id' => 'xxxxxxxxxxxxxxxx',
-            'checkin_status' => false,
             'has_credit_card' => true,
-            'book_code' => 'xxxxxxxxxxxxxxxx',
-            'checkin_at' => Carbon::yesterday(),
-            'checkout_at' => Carbon::today(),
+            'book_code' => $bookService->createBookCode(),
+            'checkin_at' => Carbon::today(),
+            'checkout_at' => Carbon::tomorrow(),
             'booked_at' => Carbon::yesterday(),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminUserSeeder::class,
-            PlanSeeder::class
+            PlanSeeder::class,
+            BookSeeder::class,
         ]);
     }
 }
